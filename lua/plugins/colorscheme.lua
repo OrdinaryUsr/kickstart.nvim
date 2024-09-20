9541f1641d3d56cb9c6493e6fc3c4c0c935fb1b0
David Samakhavets <d.samakhavets@bgapps.pl>
2024-09-20 18:55:36 +0300

    Colorscheme dim_inactive

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ return { -- You can easily change to a different colorscheme.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'folke/tokyonight.nvim',
+  lazy = false,
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
     -- Load the colorscheme here.
@@ -14,4 +15,11 @@ return { -- You can easily change to a different colorscheme.
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
   end,
+  opts = {
+    dim_inactive = true,
+    on_colors = function(colors)
+      -- colors.bg_dark = '#24283b'
+      colors.bg_dark = '#1a1b26'
+    end,
+  },
 }
